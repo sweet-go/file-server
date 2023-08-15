df1726dbf86ac1147d5bc0baf0a3a3414a013aa1
lucky akbar <luckyakbar1509@gmail.com>
2023-08-15 09:40:14 +0700

    feat: new input for delete and upload file

--- client/datatype.go
+++ client/datatype.go
@@ -8,10 +8,18 @@ import (
 
 // UploadFileInput is the input for Upload method
 type UploadFileInput struct {
-	FullPath string
+	FullPath       string
+	IsDeletable    bool
+	DeletableMedia *model.DeletableMedia
+}
+
+type DeleteFileInput struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 // Utility is the interface for client utility
 type Utility interface {
 	Upload(ctx context.Context, input UploadFileInput) (*model.File, error)
+	Delete(ctx context.Context, input *DeleteFileInput) error
 }
