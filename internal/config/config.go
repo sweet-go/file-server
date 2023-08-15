c8b19c1afffdea499a09f10d9894ea608e691af9
lucky akbar <luckyakbar1509@gmail.com>
2023-08-15 10:08:53 +0700

    feat: new config for postgres dsn

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,3 +22,15 @@ func ServerPort() string {
 func StoragePath() string {
 	return viper.GetString("server.storage_path")
 }
+
+// PostgresDSN returns postgres DSN
+func PostgresDSN() string {
+	host := viper.GetString("postgres.host")
+	db := viper.GetString("postgres.db")
+	user := viper.GetString("postgres.user")
+	pw := viper.GetString("postgres.pw")
+	port := viper.GetString("postgres.port")
+	sslMode := viper.GetString("postgres.ssl_mode")
+
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, pw, db, port, sslMode)
+}
